2694602fff5943148a3f92e2fa7eb57878dabc54
edwisdom <22333941+edwisdom@users.noreply.github.com>
2025-10-17 02:21:18 -0400

    Add a list-integrations command

--- tasks.py
+++ tasks.py
@@ -6,6 +6,7 @@ import asyncio
 from invoke import Context, task
 
 from monarch_feeder.integration_types import Platform
+from monarch_feeder.integrations import INTEGRATIONS
 from monarch_feeder.sync import sync_all
 
 
@@ -35,3 +36,14 @@ def sync(
         platform_list = [Platform(name) for name in platform_names]
 
     asyncio.run(sync_all(platforms=platform_list, dry_run=dry_run))
+
+
+@task
+def list_integrations(
+    ctx: Context,
+) -> None:
+    """
+    List all integrations.
+    """
+    platforms = [platform.value for platform in INTEGRATIONS]
+    print(f"Available platforms: {', '.join(platforms)}")
